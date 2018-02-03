1fa04807fc6be33d287b533a6ee3d6714905e8c3
JustCourt <courtney.j.siu@gmail.com>
2018-02-02 22:06:16 -0500

    Added posts and user classes

---
+++ app/src/main/java/com/veephealthoutloud/healthoutloud/Classes/Post.java
@@ -0,0 +1,11 @@
+package com.veephealthoutloud.healthoutloud.Classes;
+
+
+import com.veephealthoutloud.healthoutloud.Interfaces.IPost;
+
+/**
+ * Class representing a post.
+ */
+
+public class Post implements IPost {
+}
